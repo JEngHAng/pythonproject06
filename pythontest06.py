deea77fbf3dc9f3ae748858312d81db32c29cdf7
JEngHAng <jangg500z@gmail.com>
2023-09-22 10:47:17 +0700

    for

--- pythontest06.py
+++ pythontest06.py
@@ -2,8 +2,11 @@ for x in range(5):
     print (f"YATTA '{x}'")
 print ("---------------------END---------------------")
 for y in range(10):
-    print (f"YATTAAAAAAAAAA '{y}'")
+    print (f"YATTAA '{y}'")
 print ("---------------------END---------------------")
-for z in range( 2 , 10 ):
-    print (f"YATTAAAAAAAAAA '{z}'")
+for z in range( 2 , 10 ): #เริ่ม 2 จบ 10
+    print (f"YATTAAA '{z}'")
+print ("---------------------END---------------------")
+for w in range( 3 , 10 , 2 ): #เริ่มที่ 3 ไป 10 โดยการบวกทีละ 2
+    print (f"YATTAAAA '{w}'")
 print ("---------------------END---------------------")
