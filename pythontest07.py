d7d94fa8f1b0d9bad5ff02099da6ffd899aa38dc
JEngHAng <jangg500z@gmail.com>
2023-09-22 10:58:38 +0700

    continue : braek

---
+++ pythontest07.py
@@ -0,0 +1,11 @@
+#continue , braek
+for x in range(5):
+    if x == 3 :
+        break
+    print (f"YATTA '{x}'")
+print ("^-----Break-----^\n")
+print ("v-----continue-----v")
+for y in range(5):
+    if y == 3 :
+        continue
+    print (f"YATTAA '{y}")
