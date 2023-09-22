387a75523e80d2233d5db3b434fac2079a7f22f2
JEngHAng <jangg500z@gmail.com>
2023-09-22 10:43:06 +0700

    for

--- pythontest06.py
+++ pythontest06.py
@@ -1,10 +1,9 @@
-num = 0
-numm = 0
 for x in range(5):
-    print (f"YATTA '{num}'")
-    num = num + 1
+    print (f"YATTA '{x}'")
 print ("---------------------END---------------------")
 for y in range(10):
-    print (f"YATTAAAAAAAAAA '{numm}'")
-    numm = numm + 1
+    print (f"YATTAAAAAAAAAA '{y}'")
+print ("---------------------END---------------------")
+for z in range(10):
+    print (f"YATTAAAAAAAAAA '{z}'")
 print ("---------------------END---------------------")
